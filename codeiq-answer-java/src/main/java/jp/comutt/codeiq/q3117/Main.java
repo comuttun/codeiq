2fe0babbf9ebeb4fbe06330891875570c3feb3b8
Seiji Komatsu <comutt.jp@gmail.com>
2017-09-11 00:39:24 +0900

    Fixed answer for Q3117

--- codeiq-answer-java/src/main/java/jp/comutt/codeiq/q3117/Main.java
+++ codeiq-answer-java/src/main/java/jp/comutt/codeiq/q3117/Main.java
@@ -1,17 +1,23 @@
 package jp.comutt.codeiq.q3117;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        new LogicTest().runTest();
+        LogicTest test = new LogicTest();
+        test.runTest();
+        test.runTest2();
+
         Logic logic = new Logic();
         try (Scanner scanner = new Scanner(System.in)) {
             while (scanner.hasNextLine()) {
-                System.out.println(logic.solve(Integer.valueOf(scanner.nextLine())));
+                System.out.println(logic.solveCached(Integer.valueOf(scanner.nextLine())));
             }
         }
     }
@@ -26,19 +32,38 @@ public class Main {
         private static final int MAX_INPUT_VALUE = 1_000;
 
         private final List<Integer> SUMS = new ArrayList<>();
+        private final Map<Integer, Integer> RESULTS = new HashMap<>();
 
         public Logic() {
-            for (int i = MIN_VALUE; i < MAX_VALUE; i++) {
+            IntStream.range(MIN_VALUE, MAX_VALUE + 1).forEach(i -> {
                 SUMS.add(i * 2 + 1);
-            }
+            });
+
+            solveAll();
         }
 
-        public int solve(int inputNumber) {
+        private int solve(int inputNumber) {
             if (inputNumber < MIN_INPUT_VALUE || MAX_INPUT_VALUE < inputNumber) {
                 throw new IllegalArgumentException("inputNumber x must be " + MIN_INPUT_VALUE + " <= x <= " + MAX_INPUT_VALUE);
             }
 
-            return (int) SUMS.stream().parallel().filter(s -> s % inputNumber == 0).count();
+            if (inputNumber % 2 == 0) {
+                return 0;
+            }
+
+//            int start = (inputNumber - 1) / 2 - MIN_VALUE;
+//            return (int) SUMS.subList(start, SUMS.size()).stream().parallel().filter(s -> s % inputNumber == 0).count();
+            return (int) (1.0 * (MAX_VALUE * 2 - 1) / inputNumber / 2 + 0.5);
+        }
+
+        private void solveAll() {
+            IntStream.range(MIN_INPUT_VALUE, MAX_INPUT_VALUE + 1).parallel().forEach(i -> {
+                RESULTS.put(i, solve(i));
+            });
+        }
+
+        public int solveCached(int inputNumber) {
+            return RESULTS.get(inputNumber);
         }
 
     }
@@ -71,6 +96,22 @@ public class Main {
             assert logic.solve(545) == 1835;
             assert logic.solve(165) == 6061;
         }
+
+        public void runTest2() {
+            if (!getClass().desiredAssertionStatus()) {
+                // assertion disabled
+                return;
+            }
+
+            Logic logic = new Logic();
+            long startTime = System.currentTimeMillis();
+            for (int i = 2; i < 1_000; i++) {
+                System.out.println(logic.solve(i));
+            }
+            long endTime = System.currentTimeMillis();
+            System.out.println(endTime - startTime + " ms");
+        }
+
     }
 
 }
